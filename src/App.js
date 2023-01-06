b014a90a78754e5a1ea96472a9bc45f20045a0b3
shikhars22 <shikhars22@gmail.com>
2023-01-06 23:22:24 +0530

    Intro to React Query - React Tutorial 86 Done

--- src/App.js
+++ src/App.js
@@ -1,25 +1,21 @@
-import logo from './logo.svg';
+import { useQuery } from '@tanstack/react-query';
+import axios from 'axios';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	const { data, error, isLoading } = useQuery(['test'], () => {
+		return axios(
+			'https://api.coingecko.com/api/v3/simple/price?ids=bitcoin&vs_currencies=inr'
+		);
+	});
+
+	return (
+		<div className='App'>
+			{error ? <p>Error</p> : null}
+			{isLoading ? <p>Loading...</p> : null}
+			{data?.data?.bitcoin?.inr ? data.data.bitcoin.inr : null}
+		</div>
+	);
 }
 
 export default App;
