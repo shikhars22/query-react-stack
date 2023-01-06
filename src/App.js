ca5f8a2ffd44f9bbc68abc9e7d724ceabe164565
shikhars22 <shikhars22@gmail.com>
2023-01-07 00:52:43 +0530

    React Query Refetch Options and Dev Tools - React Tutorial 87 Done

--- src/App.js
+++ src/App.js
@@ -3,17 +3,33 @@ import axios from 'axios';
 import './App.css';
 
 function App() {
-	const { data, error, isLoading } = useQuery(['test'], () => {
-		return axios(
-			'https://api.coingecko.com/api/v3/simple/price?ids=bitcoin&vs_currencies=inr'
-		);
-	});
-
+	const priceQuery = useQuery(
+		['price'],
+		() => {
+			return axios(
+				'https://api.coingecko.com/api/v3/simple/price?ids=bitcoin&vs_currencies=inr'
+			);
+		},
+		{
+			refetchInterval: 1000 * 5,
+		}
+	);
+	const customerQuery = useQuery(
+		['customers'],
+		() => {
+			return axios('http://localhost:3002/api/customers');
+		},
+		{
+			refetchInterval: 1000 * 5,
+		}
+	);
 	return (
 		<div className='App'>
-			{error ? <p>Error</p> : null}
-			{isLoading ? <p>Loading...</p> : null}
-			{data?.data?.bitcoin?.inr ? data.data.bitcoin.inr : null}
+			{priceQuery?.error ? <p>Error</p> : null}
+			{priceQuery?.isLoading ? <p>Loading...</p> : null}
+			{priceQuery?.data?.data?.bitcoin?.inr
+				? priceQuery.data.data.bitcoin.inr
+				: null}
 		</div>
 	);
 }
--- src/index.js
+++ src/index.js
@@ -9,15 +9,23 @@ import {
 	QueryClient,
 	QueryClientProvider,
 } from '@tanstack/react-query';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 
 // Create a client
-const queryClient = new QueryClient();
+const queryClient = new QueryClient({
+	defaultOptions: {
+		queries: {
+			refetchOnWindowFocus: false,
+		},
+	},
+});
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
 	<React.StrictMode>
 		<QueryClientProvider client={queryClient}>
 			<App />
+			<ReactQueryDevtools initialIsOpen={true} />
 		</QueryClientProvider>
 	</React.StrictMode>
 );
